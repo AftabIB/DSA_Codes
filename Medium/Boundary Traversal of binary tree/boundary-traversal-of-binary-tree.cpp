181b897c24a0516f03df20158fbef8776b84c815
Aftab Bhadgaonkar <122194015+AftabIB@users.noreply.github.com>
2023-10-18 08:41:55 +0530

    Updated Solution - GfG to GitHub

--- Medium/Boundary Traversal of binary tree/boundary-traversal-of-binary-tree.cpp
+++ Medium/Boundary Traversal of binary tree/boundary-traversal-of-binary-tree.cpp
@@ -105,72 +105,87 @@ struct Node
 
 class Solution {
 public:
-    void traverseLeft(Node* root, vector<int> &ans) {
-        //base case
-        if( (root == NULL) || (root->left == NULL && root->right == NULL) )
-            return ;
-            
+void traverseLeft(Node* root,vector<int> &ans)
+{
+    //base case
+    if((root == NULL) || (root -> left == NULL && root -> right == NULL))
+        return;
+    
+    //store the data 
+    ans.push_back(root->data);
+    //if left exist , go to left 
+    if(root->left)
+    {
+        traverseLeft(root->left,ans);
+    }
+    //else, go to right
+    else
+    {
+        traverseLeft(root->right,ans);
+    }
+}
+
+void traverseLeaf(Node* root,vector<int> &ans)
+{
+    //base case
+    if(root == NULL)
+        return;
+    
+    //if leaf nodes exist, store the answer in leaf node ans return
+    if(root -> left == NULL && root -> right == NULL)
+    {
         ans.push_back(root->data);
-        if(root->left)
-            traverseLeft(root->left, ans);
-        else
-            traverseLeft(root->right, ans);
-            
+        return;
     }
     
-    void traverseLeaf(Node* root, vector<int> &ans) {
-        //base case
-        if(root == NULL)
-            return ;
-            
-        if(root->left == NULL && root->right == NULL) {
-            ans.push_back(root->data);
-            return;
-        }
-        
-        traverseLeaf(root->left, ans);
-        traverseLeaf(root->right, ans);
-        
+    traverseLeaf(root->left,ans);
+    traverseLeaf(root->right,ans);
+}
+
+void traverseRight(Node* root,vector<int> &ans)
+{
+    //base case
+    if((root == NULL) || (root -> left == NULL && root -> right == NULL))
+        return;
+    
+    //if right node exist ,go to right
+    if(root->right)
+    {
+        traverseRight(root->right,ans);
+    }
+    //else, go to left
+    else
+    {
+        traverseRight(root->left,ans);
     }
     
-    void traverseRight(Node* root, vector<int> &ans) {
-        //base case
-        if( (root == NULL) || (root->left == NULL && root->right == NULL) )
-            return ;
-        
-        if(root->right)
-            traverseRight(root->right, ans);
-        else
-            traverseRight(root->left, ans);
-            
-        //wapas aagye
-        ans.push_back(root->data);
-            
-    }
-    
+    //print the nodes in reverse format
+    ans.push_back(root->data);
+}
+
     vector <int> boundary(Node *root)
     {
         vector<int> ans;
         if(root == NULL)
             return ans;
-            
+        
+        //storing the root's data in answer
         ans.push_back(root->data);
         
-        //left part print/store
-        traverseLeft(root->left, ans);
+        //printing the left subtree except the leaf nodes
+        traverseLeft(root->left,ans);
         
-        //traverse Leaf Nodes
+        //printing left subtree leaf nodes
+        traverseLeaf(root->left,ans);
         
-        //left subtree
-        traverseLeaf(root->left, ans);
-        //right subtree
-        traverseLeaf(root->right, ans);
+        //printing right subtree leaf nodes
+        traverseLeaf(root->right,ans);
         
-        //traverse right part
-        traverseRight(root->right, ans);
+        ////printing right subtree nodes
+        traverseRight(root->right,ans);
         
         return ans;
-
+        
     }
 };
 
