056552dd2259de69df056eab5c5d8403ba6b72b5
Aftab Bhadgaonkar <122194015+AftabIB@users.noreply.github.com>
2023-10-21 15:14:55 +0530

    Time: 3 ms (78.58%), Space: 6.5 MB (9.71%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        // code here
-        if(num==1)return 0;
-        long long sum=1;
-        for(long long i=2;i<=sqrt(num);i++){
-            if(num%i==0){
-                sum+=i;
-                sum+=(num/i);
+        if (num <= 1) {
+            return false; // 1 is not a perfect number
+        }
+        
+        int sum = 1; // Start with 1 because all numbers are divisible by 1
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                if (i != num / i) {
+                    sum += num / i;
+                }
             }
         }
-        if(sum==num)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        
+        return sum == num;
     }
 };
