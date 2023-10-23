ba2d98b892ee1354dfc4868aa5e0cc7039708dba
Aftab Bhadgaonkar <122194015+AftabIB@users.noreply.github.com>
2023-10-23 08:39:20 +0530

    Time: 10 ms (72.97%), Space: 6.2 MB (65.15%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n==0)
+        {
+            return false;
+        }
+        else if(n==1)
+        {
+            return true;
+        }
+        long long x = 3;
+        while(x<=n)
+        {
+            if(x==n)    return true;
+            x *= 3;
+        }
+        return false;
+    }
+};
