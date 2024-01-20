72ee4e7ed1fabd10d418dc53feb4f48ec703b945
Aftab Bhadgaonkar <122194015+AftabIB@users.noreply.github.com>
2024-01-20 19:54:40 +0530

    Updated Solution - GfG to GitHub

--- Medium/Longest Common Subsequence/longest-common-subsequence.cpp
+++ Medium/Longest Common Subsequence/longest-common-subsequence.cpp
@@ -30,25 +30,50 @@ class Solution
     // }
     
     //Memoization
-    int solve(int index1,int index2,string &s1,string &s2,vector<vector<int>> &dp)
-    {
-        //base case 
-        if(index1 < 0 || index2 < 0)
-            return 0;
+    // int solve(int index1,int index2,string &s1,string &s2,vector<vector<int>> &dp)
+    // {
+    //     //base case 
+    //     if(index1 < 0 || index2 < 0)
+    //         return 0;
         
-        if(dp[index1][index2] != -1)
-            return dp[index1][index2];
+    //     if(dp[index1][index2] != -1)
+    //         return dp[index1][index2];
             
-        //matching case
-        if(s1[index1] == s2[index2])
-            return  dp[index1][index2] = 1 + solve(index1-1,index2-1,s1,s2,dp);
+    //     //matching case
+    //     if(s1[index1] == s2[index2])
+    //         return  dp[index1][index2] = 1 + solve(index1-1,index2-1,s1,s2,dp);
         
-        return dp[index1][index2] = max(solve(index1-1,index2,s1,s2,dp),solve(index1,index2-1,s1,s2,dp));
-    }
+    //     return dp[index1][index2] = max(solve(index1-1,index2,s1,s2,dp),solve(index1,index2-1,s1,s2,dp));
+    // }
+    // int lcs(int n, int m, string s1, string s2)
+    // {
+    //     vector<vector<int>> dp(n,vector<int>(m+1,-1));
+    //     return solve(n-1,m-1,s1,s2,dp);
+    // }
+    
+    // Tabulation
     int lcs(int n, int m, string s1, string s2)
     {
-        vector<vector<int>> dp(n,vector<int>(m+1,-1));
-        return solve(n-1,m-1,s1,s2,dp);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        
+        //base case
+        for(int j=0;j<m;j++)
+            dp[0][j] = 0;
+        for(int i=0;i<n;i++)
+            dp[i][0] = 0;
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                //matching case
+                if(s1[i-1] == s2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
     }
 };
 
