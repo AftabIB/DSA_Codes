2b6cfd8b61d456da7f6b0ea0045d5f7afefa6e0e
Aftab Bhadgaonkar <122194015+AftabIB@users.noreply.github.com>
2024-01-20 20:03:48 +0530

    Time: 30 ms (83.94%) | Memory: 25.7 MB (6.27%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //Recursion
     // int solve(int i, int j,string &text1, string &text2)
     // {
     //     //base case 
@@ -20,26 +21,52 @@ public:
     // }
 
     // memoization
-    int solve(int i, int j,string &text1, string &text2,vector<vector<int>> &dp)
-    {
-        //base case 
-        if(i < 0 || j < 0)
-            return 0;
+    // int solve(int i, int j,string &text1, string &text2,vector<vector<int>> &dp)
+    // {
+    //     //base case 
+    //     if(i < 0 || j < 0)
+    //         return 0;
 
-        if(dp[i][j] != -1)
-            return dp[i][j];
+    //     if(dp[i][j] != -1)
+    //         return dp[i][j];
         
-        //matching case
-        if(text1[i] == text2[j])
-            return dp[i][j] = 1 + solve(i-1,j-1,text1,text2,dp);
+    //     //matching case
+    //     if(text1[i] == text2[j])
+    //         return dp[i][j] = 1 + solve(i-1,j-1,text1,text2,dp);
         
-        //no matching case 
-        return dp[i][j] = max(solve(i-1,j,text1,text2,dp),solve(i,j-1,text1,text2,dp));
-    }
+    //     //no matching case 
+    //     return dp[i][j] = max(solve(i-1,j,text1,text2,dp),solve(i,j-1,text1,text2,dp));
+    // }
+    // int longestCommonSubsequence(string text1, string text2) {
+    //     int i = text1.size();
+    //     int j = text2.size();
+    //     vector<vector<int>> dp(i,vector<int>(j,-1));
+    //     return solve(i-1,j-1,text1,text2,dp);
+    // }
+
+    //Tabulation
     int longestCommonSubsequence(string text1, string text2) {
-        int i = text1.size();
-        int j = text2.size();
-        vector<vector<int>> dp(i,vector<int>(j,-1));
-        return solve(i-1,j-1,text1,text2,dp);
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        
+        //base case 
+        for(int i=0;i<n;i++)
+            dp[i][0] = 0;
+
+        for(int j=0;j<m;j++)
+            dp[0][j] = 0;
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(text1[i-1] == text2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
     }
 };
