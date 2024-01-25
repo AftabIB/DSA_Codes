e0bb4356c5de2bbc7969c0158d05e457e688a359
Aftab Bhadgaonkar <122194015+AftabIB@users.noreply.github.com>
2024-01-25 13:11:49 +0530

    Time: 4 ms (91.94%) | Memory: 12 MB (19.93%) - LeetSync

---
+++ 72-edit-distance/edit-distance.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    // int solve(int i,int j,string &s1, string &s2)
+    // {
+    //     //base case
+    //     if(i<0)
+    //         return j+1;
+        
+    //     if(j<0)
+    //         return i+1;
+
+    //     if(s1[i] == s2[j])
+    //         return solve(i-1,j-1,s1,s2);
+    //     else
+    //         return 1 + min(solve(i,j-1,s1,s2),min(solve(i-1,j,s1,s2),solve(i-1,j-1,s1,s2)));
+    // }
+    // int minDistance(string s1, string s2) {
+    //     int n = s1.size();
+    //     int m = s2.size();
+
+    //     return solve(n-1,m-1,s1,s2);
+
+    // }
+
+    //Memoization
+    int solve(int i, int j, string &s1, string &s2, vector<vector<int>> &dp)
+    {
+        // base case
+        if (i < 0)
+            return j + 1;
+
+        if (j < 0)
+            return i + 1;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        if (s1[i] == s2[j])
+            return dp[i][j] = solve(i - 1, j - 1, s1, s2, dp);
+        else
+            return dp[i][j] = 1 + min(solve(i, j - 1, s1, s2, dp), min(solve(i - 1, j, s1, s2, dp), solve(i - 1, j - 1, s1, s2, dp)));
+    }
+
+    int minDistance(string s1, string s2)
+    {
+        int n = s1.size();
+        int m = s2.size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return solve(n - 1, m - 1, s1, s2, dp);
+    }
+};
