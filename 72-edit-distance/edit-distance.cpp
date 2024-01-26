28e2597d167201358fde61fc4af355e0ef5f20d8
Aftab Bhadgaonkar <122194015+AftabIB@users.noreply.github.com>
2024-01-26 12:29:10 +0530

    Time: 12 ms (54.05%) | Memory: 12.2 MB (17.60%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -26,16 +26,16 @@ public:
     int solve(int i, int j, string &s1, string &s2, vector<vector<int>> &dp)
     {
         // base case
-        if (i < 0)
-            return j + 1;
+        if (i == 0)
+            return j;
 
-        if (j < 0)
-            return i + 1;
+        if (j == 0)
+            return i;
 
         if (dp[i][j] != -1)
             return dp[i][j];
 
-        if (s1[i] == s2[j])
+        if (s1[i-1] == s2[j-1])
             return dp[i][j] = solve(i - 1, j - 1, s1, s2, dp);
         else
             return dp[i][j] = 1 + min(solve(i, j - 1, s1, s2, dp), min(solve(i - 1, j, s1, s2, dp), solve(i - 1, j - 1, s1, s2, dp)));
@@ -45,7 +45,7 @@ public:
     {
         int n = s1.size();
         int m = s2.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return solve(n - 1, m - 1, s1, s2, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        return solve(n, m, s1, s2, dp);
     }
 };
