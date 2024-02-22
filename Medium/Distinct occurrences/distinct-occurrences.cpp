f7dedb4e3217c17f9ae1ced9ebed62ee40e8476c
Aftab Bhadgaonkar <122194015+AftabIB@users.noreply.github.com>
2024-02-22 23:39:55 +0530

    Updated Solution - GfG to GitHub

--- Medium/Distinct occurrences/distinct-occurrences.cpp
+++ Medium/Distinct occurrences/distinct-occurrences.cpp
@@ -9,63 +9,32 @@ using namespace std;
 class Solution
 {
     public:
-    // Memoization(TLE)
-    // int solve(int i,int j,string &S, string &T,vector<vector<int>> &dp)
-    // {
-    //     //base case
-    //     if(j < 0)
-    //         return 1;
-    //     if(i < 0)
-    //         return 0;
-            
-    //     if(dp[i][j] != -1)
-    //         return dp[i][j];
-                    
-    //     if(S[i] == T[j])
-    //         return solve(i-1,j-1,S,T,dp) + solve(i-1,j,S,T,dp);
-    //     else
-    //         return solve(i-1,j,S,T,dp);
-    // }
-    // int subsequenceCount(string S, string T)
-    // {
-    //     int i = S.size();
-    //     int j = T.size();
-    //     vector<vector<int>> dp(i,vector<int> (j,-1));
-    //     return solve(i-1,j-1,S,T,dp);
-    //     //Your code here
-    // }
-    
-    // Tabulation
-    int subsequenceCount(string S, string T)
-    {
-        int mod = 1e9 + 7;
-        int n = S.size();
-        int m = T.size();
-        
-        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
-        
-        for(int i=0;i<=n;i++)
-            dp[i][0] = 1;
-        
-        for(int j=1;j<=m;j++)
-            dp[0][j] = 0;
-        
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=m;j++)
-            {
-                if(S[i-1] == T[j-1])
-                {
-                    dp[i][j] = (dp[i-1][j-1] + dp[i-1][j]) % mod; 
-                }
-                else
-                {
-                    dp[i][j] = dp[i-1][j] % mod;
-                }
-            }
+    int mod=1e9+7;
+    int f(int i,int j,string &s,string &t,vector<vector<int>> &dp){
+        if(j==t.size()){
+            return 1;
+        }
+        if(i==s.size()){
+            return 0;
         }
         
-        return dp[n][m]  % mod;
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int ans=0;
+        if(s[i]==t[j]){
+            ans+=f(i+1,j+1,s,t,dp);
+        }
+        
+        ans+=f(i+1,j,s,t,dp);
+        
+        return dp[i][j]=ans%mod;
+    }
+    int subsequenceCount(string s, string t)
+    {
+      //Your code here
+      vector<vector<int>> dp(s.size(),vector<int>(t.size(),-1));
+      return f(0,0,s,t,dp);
     }
 };
  
