05f0a5bd36937bd52248ff9f8489677cb174de81
Aftab Bhadgaonkar <122194015+AftabIB@users.noreply.github.com>
2024-03-08 22:41:50 +0530

    Time: 11 ms (91.88%) | Memory: 21.5 MB (58.76%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,20 +1,52 @@
 class Solution {
 public:
-    void moveZeroes(std::vector<int>& nums) {
+    void moveZeroes(vector<int>& nums) {
         int n = nums.size();
-        int left = 0; // Pointer to keep track of the current position to place non-zero elements
+        int i = 0;
 
-        // Iterate through the array
-        for (int right = 0; right < n; right++) {
-            // If the current element is non-zero, move it to the left pointer position
-            if (nums[right] != 0) {
-                nums[left++] = nums[right];
+        for(int j=0;j<n;j++)
+        {
+            if(nums[j] != 0)
+            {
+                nums[i++] = nums[j];
             }
         }
 
-        // Fill the remaining positions with zeros
-        while (left < n) {
-            nums[left++] = 0;
+        while(i < n)
+        {
+            nums[i++] = 0;
         }
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     void moveZeroes(std::vector<int>& nums) {
+//         int n = nums.size();
+//         int left = 0; // Pointer to keep track of the current position to place non-zero elements
+
+//         // Iterate through the array
+//         for (int right = 0; right < n; right++) {
+//             // If the current element is non-zero, move it to the left pointer position
+//             if (nums[right] != 0) {
+//                 nums[left++] = nums[right];
+//             }
+//         }
+
+//         // Fill the remaining positions with zeros
+//         while (left < n) {
+//             nums[left++] = 0;
+//         }
+//     }
+// };
