3b353e374bf226a678ce38f78b951e8d21b66492
Aftab Bhadgaonkar <122194015+AftabIB@users.noreply.github.com>
2024-03-11 09:38:30 +0530

    Time: 5 ms (8.56%) | Memory: 9.5 MB (6.35%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
-    bool customComparator(char a,char b,string &s)
-    {
-        string compStr = s;
-        return compStr.find(a) < compStr.find(b);
-    }
-
     string customSortString(string order, string s) {
-        // unordered_map<char,int> mp;
-        // for(auto i:order)
-        // {
-        //     mp[order[i]]++;
-        // }
-        sort(s.begin(), s.end(), [&](char a, char b) {
-            return customComparator(a, b, order);
-        });
+        unordered_map<char, int> mp;
+        int n = order.length();
+
+        for (int i = 0; i < n; i++) {
+            mp[order[i]] = i;
+        }
+
+        auto customComparator = [&](char a, char b) {
+            return mp[a] < mp[b];
+        };
+        
+        sort(s.begin(), s.end(), customComparator);
+        
         return s;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     bool customComparator(char a,char b,string &s)
+//     {
+//         string compStr = s;
+//         return compStr.find(a) < compStr.find(b);
+//     }
+
+//     string customSortString(string order, string s) {
+//         sort(s.begin(), s.end(), [&](char a, char b) {
+//             return customComparator(a, b, order);
+//         });
+//         return s;
+//     }
+// };
