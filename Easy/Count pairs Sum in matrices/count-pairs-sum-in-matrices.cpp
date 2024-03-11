829a40796a1400f38f1897bc84635caa5b859180
Aftab Bhadgaonkar <122194015+AftabIB@users.noreply.github.com>
2024-03-11 10:18:05 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count pairs Sum in matrices/count-pairs-sum-in-matrices.cpp
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	int countPairs(vector<vector<int>> &mat1, vector<vector<int>> &mat2, int n, int x)
+	{
+	    unordered_map<int,int> mp;
+	    
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                mp.insert({mat2[i][j], 1});
+            }
+        }
+        
+        int count = 0;
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(mp.find(x - mat1[i][j]) != mp.end())
+                    count++;
+            }
+        }
+        
+        return count;
+    
+    
+        // int count = 0;
+        // for(int i = 0; i < n; i++) {
+        //     int left = 0;
+        //     int right = n - 1;
+        //     while(left < n && right >= 0) {
+        //         int sum = mat1[i][left] + mat2[i][right];
+        //         if(sum == x) {
+        //             count++;
+        //             left++;
+        //             right--;
+        //         } else if(sum < x) {
+        //             left++;
+        //         } else {
+        //             right--;
+        //         }
+        //     }
+        // }
+        // return count;
+
+
+	}
+};
+
+//{ Driver Code Starts.
+
+
+int main() 
+{
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x;
+        cin >> n >> x;
+
+        vector<vector<int>> mat1(n, vector<int>(n, -1));
+        vector<vector<int>> mat2(n, vector<int>(n, -1));
+
+        for(int i = 0; i < n; i++)
+        {
+        	for(int j = 0; j < n; j++)
+        	{
+        		cin >> mat1[i][j];
+        	}
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+        	for(int j = 0; j < n; j++)
+        	{
+        		cin >> mat2[i][j];
+        	}
+        }
+
+        Solution ob;
+
+        cout << ob.countPairs(mat1, mat2, n, x) << "\n";
+
+    }
+
+    return 0;
+}
+// } Driver Code Ends
