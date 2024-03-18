6291be521426df1bb6e536ba51d9add764a37805
Aftab Bhadgaonkar <122194015+AftabIB@users.noreply.github.com>
2024-03-18 20:34:52 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Rotate by 90 degree/rotate-by-90-degree.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+
+class Solution
+{   
+    public:
+    //Function to rotate matrix anticlockwise by 90 degrees.
+    void rotateby90(vector<vector<int> >& matrix, int n) 
+    { 
+        // code here
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        
+        
+        for(int j = 0; j < n; j++){
+            int s = 0, e = n-1;
+            while(s < e){
+                swap(matrix[s++][j], matrix[e--][j]);
+            }
+        }
+    } 
+};
+
+// 1 4 7 
+// 2 5 8
+// 3 6 9
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        vector<vector<int> > matrix(n); 
+
+        for(int i=0; i<n; i++)
+        {
+            matrix[i].assign(n, 0);
+            for( int j=0; j<n; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        ob.rotateby90(matrix, n);
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j)
+                cout<<matrix[i][j]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
