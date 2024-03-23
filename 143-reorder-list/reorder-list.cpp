86f84bc99b719a05823b50f46ac869c03689cb92
Aftab Bhadgaonkar <122194015+AftabIB@users.noreply.github.com>
2024-03-23 08:58:49 +0530

    Time: 20 ms (84.60%) | Memory: 21.3 MB (69.02%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -1,3 +1,65 @@
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     void reorderList(ListNode* head) {
+
+//         // Finding middle element of LL
+//         ListNode* slow = head;
+//         ListNode* fast = head -> next;
+
+//         while(fast != NULL)
+//         {
+//             fast = fast -> next;
+//             if(fast != NULL)
+//             {
+//                 fast = fast -> next;
+//             }
+//             slow = slow -> next;
+//         }
+
+
+//         //Reverse the LL
+//         ListNode* prev = NULL;
+//         ListNode* forward = NULL;
+//         ListNode* curr = slow -> next;
+
+//         while(curr)
+//         {
+//             forward = curr -> next;
+//             curr -> next = prev;
+//             prev = curr;
+//             curr = forward;
+//         }
+//         slow->next = NULL;    
+
+//         // Merge the 2 LL
+//         ListNode* A = head;
+//         ListNode* B = prev;
+
+//         while(A && B)
+//         {
+//             ListNode* nextA = A -> next;
+//             ListNode* nextB = B -> next;
+
+//             A -> next = B;
+//             B -> next = nextA;
+
+//             A = nextA;
+//             B = nextB;
+//         }
+//     }
+// };
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -12,49 +74,47 @@ class Solution {
 public:
     void reorderList(ListNode* head) {
 
-        // Finding middle element of LL
         ListNode* slow = head;
-        ListNode* fast = head -> next;
-
-        while(fast != NULL)
+        ListNode* fast = head;
+        while (fast != nullptr && fast->next != nullptr)
         {
-            fast = fast -> next;
-            if(fast != NULL)
-            {
-                fast = fast -> next;
-            }
-            slow = slow -> next;
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
+        ListNode* mid = slow->next;
+        slow->next = nullptr;
 
-        //Reverse the LL
-        ListNode* prev = NULL;
-        ListNode* forward = NULL;
-        ListNode* curr = slow -> next;
-
-        while(curr)
+        ListNode* prev = nullptr;
+        for (ListNode* current = mid; current != nullptr ;)
         {
-            forward = curr -> next;
-            curr -> next = prev;
-            prev = curr;
-            curr = forward;
+            ListNode* tmp = current->next;
+            current->next = prev;
+            prev = current;
+            current = tmp;
         }
-        slow->next = NULL;    
 
-        // Merge the 2 LL
-        ListNode* A = head;
-        ListNode* B = prev;
+        ListNode *current = head;
 
-        while(A && B)
+        while (prev != nullptr)
         {
-            ListNode* nextA = A -> next;
-            ListNode* nextB = B -> next;
-
-            A -> next = B;
-            B -> next = nextA;
-
-            A = nextA;
-            B = nextB;
+            ListNode* tmp1 = current->next;
+            ListNode* tmp2 = prev->next;
+            current->next = prev;
+            prev->next = tmp1;
+            prev = tmp2;
+            current = current->next->next;
         }
     }
+
+private:
+void print(ListNode* node)
+{
+    while (node != nullptr)
+    {
+        std::cout << node->val << " ";
+        node = node->next;
+    }
+    std::cout << std::endl;
+}
 };
