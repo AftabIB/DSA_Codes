4085d5aeed34a6af767b24f98794beede6b53109
Aftab Bhadgaonkar <122194015+AftabIB@users.noreply.github.com>
2024-06-24 12:07:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Median of 2 Sorted Arrays of Different Sizes/median-of-2-sorted-arrays-of-different-sizes.cpp
+++ Difficulty: Hard/Median of 2 Sorted Arrays of Different Sizes/median-of-2-sorted-arrays-of-different-sizes.cpp
@@ -9,49 +9,126 @@ double MedianOfArrays(vector<int>& array1, vector<int>& array2);
 // } Driver Code Ends
 //User function Template for C++
 
+
+// brute force
+
+    // double MedianOfArrays(vector<int>& array1, vector<int>& array2)
+    // {
+    //     // Your code goes here
+    //     vector<int> arr;
+    //     int n1 = array1.size(), n2 = array2.size();
+    //     int i = 0, j = 0;
+        
+    //     while(i < n1 && j < n2)
+    //     {
+    //         if(array1[i] < array2[j])
+    //         {
+    //             arr.push_back(array1[i]);
+    //             i++;
+    //         }
+    //         else
+    //         {
+    //             arr.push_back(array2[j]);
+    //             j++;
+    //         }
+    //     }
+        
+    //     while(i < n1)
+    //     {
+    //         arr.push_back(array1[i]);  
+    //         i++;
+    //     }
+        
+    //     while(j < n2)
+    //     {
+    //         arr.push_back(array2[j]);  
+    //         j++;
+    //     }
+        
+    //     int n = (n1 + n2);
+        
+    //     if(n%2 == 1)
+    //     {
+    //         return arr[n/2];
+    //     }
+        
+    //     return (double)((double)(arr[n/2]) + (double)(arr[n/2 - 1])) / 2.0;
+    // }
+    
+
 class Solution{
     public:
     double MedianOfArrays(vector<int>& array1, vector<int>& array2)
     {
         // Your code goes here
-        vector<int> arr;
-        int n1 = array1.size(), n2 = array2.size();
+        int n1 = array1.size();
+        int n2 = array2.size();
+        
         int i = 0, j = 0;
+        
+        int n = (n1 + n2);
+        int ind2 = n/2;
+        int ind1 = ind2 - 1;
+        
+        int indEle1 = -1, indEle2 = -1; 
+        int cnt = 0;
         
         while(i < n1 && j < n2)
         {
             if(array1[i] < array2[j])
             {
-                arr.push_back(array1[i]);
+                if(cnt == ind1)
+                    indEle1 = array1[i];
+                    
+                if(cnt == ind2)
+                    indEle2 = array1[i];
+                    
+                cnt++;
                 i++;
             }
             else
             {
-                arr.push_back(array2[j]);
+                if(cnt == ind1)
+                    indEle1 = array2[j];
+                    
+                if(cnt == ind2)
+                    indEle2 = array2[j];
+                    
+                cnt++;
                 j++;
             }
         }
         
         while(i < n1)
         {
-            arr.push_back(array1[i]);  
+            if(cnt == ind1)
+                    indEle1 = array1[i];
+                    
+            if(cnt == ind2)
+                indEle2 = array1[i];
+                    
+            cnt++;
             i++;
         }
         
         while(j < n2)
         {
-            arr.push_back(array2[j]);  
+            if(cnt == ind1)
+                indEle1 = array2[j];
+                    
+            if(cnt == ind2)
+                indEle2 = array2[j];
+                    
+            cnt++;
             j++;
         }
         
-        int n = (n1 + n2);
-        
-        if(n%2 == 1)
+        if(n % 2 == 1)
         {
-            return arr[n/2];
+            return indEle2;
         }
         
-        return (double)((double)(arr[n/2]) + (double)(arr[n/2 - 1])) / 2.0;
+        return (double)((double)(indEle1 + indEle2)) / 2.0;
     }
 };
 
